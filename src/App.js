897c9f4fa558b1239272dbafc4f420c97db304bb
Bee <ardelmbiplang@gmail.com>
2020-06-25 11:16:27 +0100

    Dashboard UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './components/homepage/homepage.component';
 import YearUpdate from './components/yearUpdate/yearUpdate.component';
 import NameUpdate from './components/nameUpdate/nameUpdate.component';
 import BioUpdate from './components/bioUpdate/bioUpdate.component';
+import Dashboard from './containers/dashboard/dashboard.container';
 import './App.css';
 
 class App extends React.Component {
@@ -18,6 +19,7 @@ class App extends React.Component {
           <Route exact path='/update-year' component={ YearUpdate }/>
           <Route exact path='/update-name' component={ NameUpdate } />
           <Route exact path='/update-bio' component={ BioUpdate } />
+          <Route exact path='/dashboard' component={ Dashboard } />
         </Switch>
       </div>
     );
